351103737b7d131e70065d2592218f3e255ea7b4
luobaoding <luobaoding@quklive.com>
2019-09-26 21:32:02 +0800

    refactor

--- integration-b-local/src/main/java/com/practice/multimodule/service_a/integration/b/local/client/ServiceBLocalClientImpl.java
+++ integration-b-local/src/main/java/com/practice/multimodule/service_a/integration/b/local/client/ServiceBLocalClientImpl.java
@@ -1,20 +1,22 @@
 package com.practice.multimodule.service_a.integration.b.local.client;
 
 import com.practice.multimodule.service_a.integration.b.spi.ServiceBClient;
-import com.practice.multimodule.service_b.endpoint.ServiceBController;
+import com.practice.multimodule.service_b.service.ServiceBService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ServiceBLocalClientImpl implements ServiceBClient {
 
-    private final ServiceBController serviceBController;
 
-    public ServiceBLocalClientImpl(ServiceBController serviceBController) {
-        this.serviceBController = serviceBController;
+    private final ServiceBService serviceBService;
+
+    public ServiceBLocalClientImpl(ServiceBService serviceBService) {
+        this.serviceBService = serviceBService;
     }
+
 
     @Override
     public String hello(String username) {
-        return serviceBController.hello(username);
+        return serviceBService.hello(username);
     }
 }
