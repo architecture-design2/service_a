c4575853080ca656269e0e1954b9eb0c0e58fcc8
luobaoding <luobaoding@quklive.com>
2019-09-27 08:49:49 +0800

    update modules

--- integration-b-local/src/main/java/com/practice/multimodule/a/integration/b/local/IntegrationLocalConfiguration.java
+++ integration-b-local/src/main/java/com/practice/multimodule/a/integration/b/local/IntegrationLocalConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "com.practice.multimodule.service_b")
+@ComponentScan(basePackages = "com.practice.multimodule.b")
 public class IntegrationLocalConfiguration {
 
 
